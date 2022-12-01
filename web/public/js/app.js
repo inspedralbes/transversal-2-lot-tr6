b00bc4af014c447aaadccc62acc5570f51e82568
unknown <a21ismalgelo@inspedralbes.cat>
2022-12-01 12:47:47 +0100

    Pagina categoria i dificultad completada

--- web/public/js/app.js
+++ web/public/js/app.js
@@ -14,25 +14,38 @@ const quiz = Vue.component('quiz', {
 const difficulty = Vue.component('difficulty', {
     data: function () {
         return {
-            categories: [],
+            categoriaSeleccionada: '',
+            dificultadSeleccionada: ''
         }
     },
     template: `
         <div class="play"> 
             <div class="setParameters">
                 <div class="categories">
-                    <label> Difficulty 
-                        <b-form-select v-model="selected" :options="categories"></b-form-select>
+                    <label> Category: 
+                        <b-form-select v-model="categoriaSeleccionada" >
+                            <option disabled selected>Please select a category</option>
+                            <option  value="arts_and_literature">Arts & Literature</option>
+                            <option  value="film_and_tv">Film & TV</option>
+                            <option  value="food_and_drink">Food & Drink</option>
+                            <option  value="general_knowledge">General Knowledge</option>
+                            <option  value="geography">Geography</option>
+                            <option  value="history">History</option>
+                            <option  value="music">Music</option>
+                            <option  value="science">Science</option>
+                            <option  value="society_and_culture">Society & Culture</option>
+                            <option  value="sport_and_leisure">Sport & Leisure</option>
+                        </b-form-select>
                     </label>
                 </div>
                 <div class="difficulty">
-                    <label> Difficulty 
-                        <select>
+                    <label> Difficulty: 
+                        <b-form-select v-model="dificultadSeleccionada">
                             <option disabled selected>Choose a difficulty</option>
                             <option value="easy">Easy</option>
                             <option value="medium">Medium</option>
                             <option value="hard">Hard</option>
-                        </select>
+                        </b-form-select>
                     </label>
                 </div>
                 <router-link to="/play" class="button">Play</router-link>
@@ -40,9 +53,11 @@ const difficulty = Vue.component('difficulty', {
         </div>`,
     mounted() {
         {
-            fetch('https://the-trivia-api.com/api/categories')
+          /*  fetch('https://the-trivia-api.com/api/categories')
                 .then((response) => response.json())
-                .then((categories) => this.categories = categories);
+                .then((categories) => 
+                {this.categories = categories;
+                console.log(categories);});*/
         }
     }
 });
